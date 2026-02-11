b05352c7d6964d3c28e7154f977d0c90e92dcf66
ritikverma127 <rv1338739@gmail.com>
2026-02-11 10:08:48 +0530

    add facebook url

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Header from "./common/Herder";
 import { Geist_Mono, Poppins } from "next/font/google";
 import Footer from "./common/Footer";
+import Script from "next/script";
+import Image from "next/image";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -38,6 +40,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '355090923902671');
+          fbq('track', 'PageView');
+        `}
+        </Script>
+
+        <noscript>
+          <Image
+            height={1}
+            width={1}
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=355090923902671&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </head>
       <body
         className={`${poppins.variable} ${geistMono.variable}  antialiased`}
       >
