ab31a0038c8548633dd6e021816c9bffbb461f58
ritikverma127 <rv1338739@gmail.com>
2026-02-21 11:04:00 +0530

    add google console and google analytics and microsoft crelarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,36 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "tpzkuwilse");
+  `}
+        </Script>
+
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-N5PQ18S5RM`}
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', 'G-N5PQ18S5RM', {
+      page_path: window.location.pathname,
+    });
+  `}
+        </Script>
+
+        {/* Meta Pixel */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
           !function(f,b,e,v,n,t,s)
